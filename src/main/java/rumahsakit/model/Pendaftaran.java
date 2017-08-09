309db1f22b3ef6f900aa0550f678ebca1118faab
ASUS <ASUS@10.8.106.57>
2017-08-09 10:50:39 +0700

    update pendaftaran & pemeriksaan

--- src/main/java/rumahsakit/model/Pendaftaran.java
+++ src/main/java/rumahsakit/model/Pendaftaran.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,5 +18,7 @@ public class Pendaftaran {
 	private int idpoli;
 	private int idpetugas;
 	private String nodaftar;
+	@OneToOne
+	private Pemeriksaan pemeriksaan;
 
 }
